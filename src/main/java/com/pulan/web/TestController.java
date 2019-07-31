d8b86a3d30dccc83f751b5c1a7700380fc6b2fce
jiangp <121670142@qq.com>
2019-07-31 14:23:13 +0800

    3 test

--- src/main/java/com/pulan/web/TestController.java
+++ src/main/java/com/pulan/web/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @RequestMapping
     public String hi() {
-        return "HI,morning!My second commit!";
+        return "HI,morning!My second commit!I've report.";
     }
 }
